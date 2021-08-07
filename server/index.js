424214c075891444015235e129d315bce5cf8fa8
Joseph Vargas <joseph@josvar.com>
2021-08-06 18:19:49 -0600

    full server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ensure_ctype = require("express-ensure-ctype");
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,10 @@ app.use(express.static("lab1"));
 app.use(bodyParser.json());
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
+// Ensure json
+const ensure_json = ensure_ctype("json");
+
+// The fake database
 spaces = [];
 reservations = [];
 space_autoincrement = 0;
@@ -35,14 +40,28 @@ function set_space(req, res, next) {
   next();
 }
 
+// Set reservation middleware
+function set_reservation(req, res, next) {
+  const { id } = req.params;
+  const reservation_index = find_reservation(id);
+
+  if (reservation_index === -1) {
+    return res.status(404).send("Reservation not found");
+  }
+
+  req.reservation = reservation_index;
+
+  next();
+}
+
 // serve spaces as json
 app.get("/spaces", (req, res) => {
   res.json(spaces);
 });
 
 // post new space
-app.post("/spaces", (req, res) => {
-  let newSpace = { ...req.body, id: space_autoincrement++ };
+app.post("/spaces", ensure_json, (req, res) => {
+  newSpace = { ...req.body, id: space_autoincrement++ };
   spaces.push(newSpace);
   res.status(201).send(newSpace);
 });
@@ -54,14 +73,36 @@ app.delete("/spaces/:id", set_space, (req, res) => {
 });
 
 // update space
-app.put("/spaces/:id", set_space, (req, res) => {
+app.put("/spaces/:id", set_space, ensure_json, (req, res) => {
   current_space = spaces[req.space];
   new_space = { ...current_space, ...req.body };
-  spaces[req.space_index] = new_space;
+  spaces[req.space] = new_space;
   res.status(200).send(new_space);
 });
 
 // get space
 app.get("/spaces/:id", set_space, (req, res) => {
   res.json(spaces[req.space]);
+});
+
+// serve reservations as json
+app.get("/reservations", (req, res) => {
+  res.json(reservations);
+});
+
+// post new reservation
+app.post("/reservations", ensure_json, (req, res) => {
+  newReservation = {
+    ...req.body,
+    id: reservations_autoincrement++,
+    time: new Date(),
+  };
+  reservations.push(newReservation);
+  res.status(201).send(newReservation);
+});
+
+// delete reservation
+app.delete("/reservations/:id", set_reservation, (req, res) => {
+  reservations.splice(req.reservation, 1);
+  res.sendStatus(200);
 });

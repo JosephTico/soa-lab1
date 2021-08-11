f467657d8d746aff827d61c6fcddbbbacc2675cb
Daniel <daniel.esteban.8@hotmail.com>
2021-08-10 23:00:06 -0600

    get and create req

--- parking-system/src/App.vue
+++ parking-system/src/App.vue
@@ -15,10 +15,48 @@ import axios from "axios";
 const spaces = ref([]);
 const reservations = ref([]);
 
+// List of Spaces
+// GET /spaces
 const getSpaces = async () => {
   const response = await axios.get("http://localhost:3000/spaces");
   spaces.value = response.data;
 };
+
+// Ask for space state
+// GET /spaces/{id} 
+
+// Create Space
+// POST /spaces
+const createSpace = async (descripcion) => {
+  const data = { "description": descripcion };
+  const response = await axios.post("http://localhost:3000/spaces", data);
+  getSpaces();
+};
+
+// Modify Space Data
+// PUT /spaces/{id} 
+
+// Delete Space
+// DELETE /spaces/{id} 
+
+// List of Reservations
+// GET /reservations
+const getReservations = async () => {
+  const response = await axios.get("http://localhost:3000/reservations");
+  reservations.value = response.data;
+};
+
+// Book an Space
+// POST /reservations
+const bookSpace = async (plate) => {
+  const data = { "plate": plate };
+  const response = await axios.post("http://localhost:3000/reservations", data);
+  getReservations();
+};
+
+// Delete Reservation
+// DELETE /reservations/{id}
+
 
 onMounted(getSpaces);
 </script>
